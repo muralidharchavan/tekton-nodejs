d7404b2e4c78e1011fd6a82b76434c888a33bd23
Manojjahgirdar <jahgirdarmanoj@gmail.com>
2020-02-07 14:00:00 +0530

    Weather app code added in server/server.js and public/index.html

--- server/server.js
+++ server/server.js
@@ -17,17 +17,52 @@ require('./routers/index')(app, server);
 
 // Add your code here
 
+const apikey = "2276454694917418ea5bacdffa49e101";
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('/getWeather', (req, res) => {
+    var lat = req.query.lat;
+    var lon = req.query.lon;
+    var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apikey;
+
+    var options = {
+        url: url
+    };
+
+    request.post(options, function(error, response, body) {
+        if (!error) {
+            requestsCount++;
+            console.log("API Called " + requestsCount + " time (s)");
+
+            if (debug) {
+                console.log("\n::: Response from Weather Api :::\n");
+                console.log(body);
+                console.log("\n::: ------------------------- :::\n");
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': body.length });
+            res.write(body);
+            res.end();
+        } else
+            console.log("erroror Occured: " + error);
+    });
+});
+
+
 const port = process.env.PORT || localConfig.port;
-server.listen(port, function(){
-  logger.info(`node listening on http://localhost:${port}`);
+server.listen(port, function() {
+    logger.info(`node listening on http://localhost:${port}`);
 });
 
-app.use(function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../public', '404.html'));
+app.use(function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../public', '404.html'));
 });
 
-app.use(function (err, req, res, next) {
-	res.sendFile(path.join(__dirname, '../public', '500.html'));
+app.use(function(err, req, res, next) {
+    res.sendFile(path.join(__dirname, '../public', '500.html'));
 });
 
 module.exports = server;
